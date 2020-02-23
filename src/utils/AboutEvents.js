1a8cb0fdd952052a8212973f25f01f39c3751346
Dan Koenig <dkoenig@gongos.com>
2020-02-23 17:00:33 -0500

    media queries written added about content

---
+++ src/utils/AboutEvents.js
@@ -0,0 +1,124 @@
+import Utilities from './Utilities.js'
+const AboutEvents = [
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 1975,
+    end: 1975,
+    month: 3,
+    details: {
+      side: 'left',
+      label: '<span class="icon-baby"></span>',
+      summary: '<p>Birth</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 1982,
+    end: 1982,
+    month: 3,
+    details: {
+      side: 'right',
+      label: '<span class="icon-lightbulb"></span>',
+      summary: '<p>Discovered love of creating things</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 1985,
+    end: 1985,
+    month: 3,
+    details: {
+      side: 'left',
+      label: '<span class="icon-d20"></span>',
+      summary: '<p>Started playing D & D</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 1987,
+    end: 1987,
+    month: 3,
+    details: {
+      side: 'right',
+      label: '<span class="icon-alien"></span>',
+      summary: '<p>Read Barlow&#39;s Guide to Extra Terrestrials</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 1991,
+    end: 1991,
+    month: 3,
+    details: {
+      side: 'left',
+      label: '<span class="icon-lips"></span>',
+      summary: '<p>First kiss</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 1997,
+    end: 1997,
+    month: 3,
+    details: {
+      side: 'right',
+      label: '<span class="icon-heart"></span>',
+      summary: '<p>Met Vicky</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 2002,
+    end: 2002,
+    month: 3,
+    details: {
+      side: 'left',
+      label: '<span class="icon-yingyang"></span>',
+      summary: '<p>Transition from designer to developer.</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 2004,
+    end: 2004,
+    month: 3,
+    details: {
+      side: 'right',
+      label: '<span class="icon-Ring"></span>',
+      summary: '<p>Got Married</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 2009,
+    end: 2009,
+    month: 3,
+    details: {
+      side: 'left',
+      label: '<span class="icon-phone"></span>',
+      summary: '<p>Put first app on app-store</p>'
+    }
+  },
+  {
+    id: Utilities.generateID(),
+    stateVal: 0,
+    start: 2017,
+    end: 2017,
+    month: 3,
+    details: {
+      side: 'right',
+      label: '<span class="icon-book"></span>',
+      summary: '<p>Published first novel</p>'
+    }
+  }
+]
+export default AboutEvents
