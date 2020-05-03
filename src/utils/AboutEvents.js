77b712cb9f69a48518847ce0fc73bcbd1a5ec2c6
Dan Koenig <dkoenig@gongos.com>
2020-05-03 10:48:10 -0400

    Signed-off-by: Dan Koenig <dkoenig@gongos.com>

--- src/utils/AboutEvents.js
+++ src/utils/AboutEvents.js
@@ -9,7 +9,7 @@ const AboutEvents = [
     details: {
       side: 'left',
       label: '<span class="icon-baby"></span>',
-      summary: '<p>Birth</p><p>I was born on March 23rd of 1975. I share my birthday with famous tennis pro Rita Grande and famous turkish rocker Burak Gürpınar. My Eyes Adored You by Frankie Valli was topping the charts and people were lining up at the theater to watch the film Night Moves.</p>'
+      summary: '<p>Birth</p><p>I was born on March 23rd of 1975. I share my birthday with famous tennis pro Rita Grande and famous Turkish rocker Burak Gürpınar. My Eyes Adored You by Frankie Valli was topping the charts and people were lining up at the theater to watch the film Night Moves.</p>'
     }
   },
   {
@@ -21,7 +21,7 @@ const AboutEvents = [
     details: {
       side: 'right',
       label: '<span class="icon-lightbulb"></span>',
-      summary: '<p>Discovered Love of Creating Things</p><p>I was seven when an elementary school rivalry drove me to enter a drawing contest. I had never made a serious attempt at drawing anything and had no reason to believe I would do well. I did not even win. In fact, I was a accused of cheating even though I did not. At first I was crushed but I soon realized that the accusation meant they did not believe a seven year old could have created what I did. I would be lying if I did not say it did&#39;t go to my head.</p>'
+      summary: '<p>Discovered Love of Creating Things</p><p>I was seven when an elementary school rivalry drove me to enter a drawing contest. I had never made a serious attempt at drawing anything and had no reason to believe I would do well. I did not even win. In fact, I was accused of cheating even though I did not. At first, I was crushed but I soon realized that the accusation meant they did not believe a seven-year-old could have created what I did. I would be lying if I did not say it didn&#39;t go to my head.</p>'
     }
   },
   {
@@ -33,7 +33,7 @@ const AboutEvents = [
     details: {
       side: 'left',
       label: '<span class="icon-d20"></span>',
-      summary: '<p>Started Playing D&D</p><p>I am not just a nerd at work. I geek out even in my off hours and I believe I owe a great deal of my nerdly nature to the late great Gary Gygax.</p>'
+      summary: '<p>Started Playing D&D</p><p>I am not just a nerd at work. I geek out even in my off hours and I believe I owe a great deal of my nerdy nature to the late great Gary Gygax.</p>'
     }
   },
   {
@@ -45,7 +45,7 @@ const AboutEvents = [
     details: {
       side: 'right',
       label: '<span class="icon-alien"></span>',
-      summary: '<p>Read <a href="https://www.amazon.com/Barlowes-Guide-Extraterrestrials-Douglas-Barlowe/dp/0894805002/ref=sr_1_1?dchild=1&keywords=read+barlow%27s+guide+to+extraterrestrials&qid=1588465175&sr=8-1" target="_blank">Barlow&#39;s Guide to Extra Terrestrials</a></p><p>This is one of the most underated books ever written. It was a gift for my father and it effected me profoundly. It showed me a window into the world of storytelling I had never considered.</p>'
+      summary: '<p>Read <a href="https://www.amazon.com/Barlowes-Guide-Extraterrestrials-Douglas-Barlowe/dp/0894805002/ref=sr_1_1?dchild=1&keywords=read+barlow%27s+guide+to+extraterrestrials&qid=1588465175&sr=8-1" target="_blank">Barlow&#39;s Guide to Extra Terrestrials</a></p><p>This is one of the most underated books ever written. It was a gift from my father and it affected me profoundly. It showed me a window into the world of storytelling I had never considered.</p>'
     }
   },
   {
@@ -57,7 +57,7 @@ const AboutEvents = [
     details: {
       side: 'left',
       label: '<span class="icon-lips"></span>',
-      summary: '<p>First Kiss</p><p>Was it love? No.</p><p>>Was it awkward? Yes.</p><p>It happened in the janitors closet of my synagogue and her name was Jenny. We never spoke of it again.</p>'
+      summary: '<p>First Kiss</p><p>Was it love? No.</p><p>>Was it awkward? Yes.</p><p>It happened in the janitor’s closet of my synagogue and her name was Jenny. We never spoke of it again.</p>'
     }
   },
   {
@@ -81,7 +81,7 @@ const AboutEvents = [
     details: {
       side: 'left',
       label: '<span class="icon-yingyang"></span>',
-      summary: '<p>Transition From Designer to Developer</p><p>I was working for a company that did alot of annual reports. The day came when we needed to create a digital version. At the time, the state of the art delivery method was a CD Rom. What I thought would be an educational departure from my day to day work would become an entirely new career path. I never looked back.</p>'
+      summary: '<p>Transition From Designer to Developer</p><p>I was working for a company that did a lot of annual reports. The day came when we needed to create a digital version. At the time, the state-of-the-art delivery method was a CD Rom. What I thought would be an educational departure from my day to day work would become an entirely new career path. I never looked back.</p>'
     }
   },
   {
@@ -93,7 +93,7 @@ const AboutEvents = [
     details: {
       side: 'right',
       label: '<span class="icon-Ring"></span>',
-      summary: '<p>Got Married</p><p>On July 11th of 2004 I married the woman I love. That&#39;s right 7/11. Fortunately for me my annaversary is very easy to remember.</p>'
+      summary: '<p>Got Married</p><p>On July 11th of 2004, I married the woman I love. That&#39;s right, 7/11. Fortunately for me, my anniversary is very easy to remember.</p>'
     }
   },
   {
@@ -105,7 +105,7 @@ const AboutEvents = [
     details: {
       side: 'left',
       label: '<span class="icon-phone"></span>',
-      summary: '<p>Put First App on App-Store</p><p>My first app was a very simple game called Myan Code. It was apattern matching game that tried to capitalize on fascination with the Myan Calendar leading up to what was to be the end of the world in 2012. I have since moved on to apps that employ Augmented Reality. One of my more recent apps is Jade Gorilla, an app that accompanies Augmented Reality greeting cards.</p>'
+      summary: '<p>Put First App on App-Store</p><p>My first app was a very simple game called Mayan Code. It was a pattern matching game that tried to capitalize on fascination with the Mayan Calendar, leading up to what was to be the end of the world in 2012. I have since moved on to apps that employ Augmented Reality. One of my more recent apps is Jade Gorilla, an app that accompanies Augmented Reality with greeting cards.</p>'
     }
   },
   {
